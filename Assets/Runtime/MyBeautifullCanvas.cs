a19018ba00bb7ad00675b16a4d4dcb16b19f439c
yodan <yodanbrandon@gmail.com>
2024-10-28 11:33:29 +0100

    fonction

--- Assets/Runtime/MyBeautifullCanvas.cs
+++ Assets/Runtime/MyBeautifullCanvas.cs
@@ -1,16 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyBeautifullCanvas : MonoBehaviour
 {
     [SerializeField] string _textview;
     [SerializeField] TextMeshProUGUI _monBeauText;
+    [SerializeField] Button _onClick;
 
     private void Start()
     {
         //_monBeauText.text = _textview;
         SetText(_textview);
+        _onClick.onClick.AddListener(DebugText);
     }
 
     public void SetText(string text) => _monBeauText.text = text;
+    public void DebugText() => Debug.Log("tu as appeler" + _monBeauText);
 }
